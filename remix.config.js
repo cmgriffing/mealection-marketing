5ffa8a5587d610ce5f82427a84f565eadddce39b
Chris Griffing <cmgriffing@gmail.com>
2022-10-02 15:12:34 -0700

    fix: remove mdx hook from config

--- remix.config.js
+++ remix.config.js
@@ -3,35 +3,7 @@
  */
 module.exports = {
   appDirectory: "app",
-  // watchPaths: ["../../packages/remark-remix-image"],
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
   serverBuildDirectory: "api/build",
-  mdx: async (filename) => {
-    const [
-      rehypeHighlight,
-      remarkToc,
-      rehypeRaw,
-      remarkRemixImage,
-      remarkMdxFrontmatter,
-    ] = await Promise.all([
-      import("rehype-highlight").then((mod) => mod.default),
-      import("remark-toc").then((mod) => mod.default),
-      import("rehype-raw").then((mod) => mod.default),
-      import("remark-remix-image").then((mod) => mod.default),
-      import("remark-mdx-frontmatter").then((mod) => mod.default),
-    ]);
-
-    return {
-      remarkPlugins: [
-        // remarkMdxFrontmatter(),
-        remarkToc,
-        // remarkRemixImage,
-      ],
-      rehypePlugins: [
-        rehypeHighlight,
-        //   rehypeRaw,
-      ],
-    };
-  },
 };
